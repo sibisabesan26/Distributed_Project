260633729ef0b67847337df2f8cefb5818f18c7c
Sibi Sabesan <64496976+sibisabesan26@users.noreply.github.com>
2025-11-22 11:15:05 -0500

    Delete Project_SampleTest directory

--- Project_SampleTest/message.py
+++
@@ -1,6 +0,0 @@
-class Message:
-    def __init__(self, sender, content, timestamp):
-        self.sender = sender
-        self.content = content
-        self.timestamp = timestamp
-        self.acks = set()
